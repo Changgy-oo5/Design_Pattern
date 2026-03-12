00500fc92eff1001ca6805307e12eec0a86f31bf
Changg𐤀 <vothikieutrang15042k5@gmail.com>
2026-03-12 21:33:24 +0700

    Delete Singleton directory

--- Singleton/Singleton/factory/ChocolateCandy.java
+++
@@ -1,9 +0,0 @@
-package com.example.designpatterns.factory;
-
-public class ChocolateCandy implements Candy {
-
-    @Override
-    public void prepare() {
-        System.out.println("Preparing chocolate candy");
-    }
-}
--- Singleton/Singleton/factory/GummyCandy.java
+++
@@ -1,9 +0,0 @@
-package com.example.designpatterns.factory;
-
-public class GummyCandy implements Candy {
-
-    @Override
-    public void prepare() {
-        System.out.println("Preparing gummy candy");
-    }
-}
