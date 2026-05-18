50fac1999ee4c7e506be32325340eca431eede27
Changg𐤀 <vothikieutrang15042k5@gmail.com>
2026-05-18 19:42:13 +0700

    Add files via upload

---
+++ Strategy/source_code/PaymentStrategy.java
@@ -0,0 +1,5 @@
+package pattern;
+
+public interface PaymentStrategy {
+    void pay (double amount);
+}
---
+++ Strategy/source_code/PaypalPayment.java
@@ -0,0 +1,8 @@
+package pattern;
+
+public class PaypalPayment implements PaymentStrategy{
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using Paypal");
+    }
+}
